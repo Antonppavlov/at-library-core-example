de724978e66db72d442420b192d644cbd786a854
Antonppavlov <anton.it.pavlov@gmail.com>
2019-04-23 18:14:38 +0300

    commit

---
+++ src/test/java/ru/bcs/test/demo/AndroidRemoteApkTest.java
@@ -0,0 +1,51 @@
+package ru.bcs.test.demo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+import static ru.bcs.test.demo.DemoTest.takeScreenshot;
+
+
+public class AndroidRemoteApkTest {
+
+    private RemoteWebDriver driver;
+
+    @Before
+    public void openDriver() throws Exception {
+        final DesiredCapabilities device = new DesiredCapabilities();
+        device.setCapability("browserName", "Nexus 5X API 24");
+//        device.setCapability("version", "7.0");
+        device.setCapability("app", "http://ci.example.com/game2048.apk"); //APK from https://www.apkmirror.com/apk/androbaby/2048/
+        device.setCapability("appPackage", "com.androbaby.game2048");
+        device.setCapability("appActivity", "com.androbaby.game2048.MainActivity");
+        device.setCapability("enableVNC", true);
+//        device.setCapability("enableVideo", true); // Uncomment this to record video
+
+        driver = new RemoteWebDriver(new URL(
+                "http://test:test-password@selenoid.t-global.bcs:4444/wd/hub" //Replace with correct host and port
+        ), device);
+    }
+
+    @Test
+    public void browserTest() throws Exception {
+        try {
+            driver.findElement(By.xpath("//*[@text=\"Start Game\"]")).click();
+        } finally {
+            takeScreenshot(driver);
+        }
+    }
+
+    @After
+    public void closeDriver(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
