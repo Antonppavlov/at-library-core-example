8b65f932e7a1468745ddc9583116ba192806c474
Antonppavlov <anton.it.pavlov@gmail.com>
2019-06-04 07:49:42 +0300

    commit

--- src/test/java/ru/bcs/test/demo/dolgov/android/AndroidDriversMethods.java
+++ src/test/java/ru/bcs/test/demo/dolgov/android/AndroidDriversMethods.java
@@ -1,26 +1,32 @@
 //package ru.bcs.test.demo.dolgov.android;
 //
 //
+//import com.codeborne.selenide.Configuration;
 //import io.appium.java_client.AppiumDriver;
 //import io.appium.java_client.android.AndroidDriver;
 //import org.junit.Assert;
 //import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
 //
 //import java.net.MalformedURLException;
+//import java.net.URI;
 //import java.net.URL;
+//
+//import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 //
 //public class AndroidDriversMethods {
 //    public static AppiumDriver<WebElement> driver;
-//    public static DesiredCapabilities cap;
 //    public static void androidLounchApp() throws MalformedURLException {
-//        cap = new DesiredCapabilities();
-//        cap.setCapability("platformName", "Android");
-//        cap.setCapability("deviceName", "POCOPHONE F1");
-//        cap.setCapability("platformVersion", "9");
-//        cap.setCapability("app", "/Users/ivanivanov/Downloads/ru.bcs.premier_2019-03-23.apk");
-//        driver = new AndroidDriver<WebElement>(new URL("http://test:test-password@selenoid.t-global.bcs:4444/wd/hub"),cap);
-//        Assert.assertNotNull(driver);
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability("platformName", "Android");
+//        desiredCapabilities.setCapability("deviceName", "POCOPHONE F1");
+//        desiredCapabilities.setCapability("platformVersion", "9");
+//        desiredCapabilities.setCapability("app", "/Users/ivanivanov/Downloads/ru.bcs.premier_2019-03-23.apk");
+//        setWebDriver(
+//                new RemoteWebDriver(
+//                        URI.create("http://test:test-password@selenoid.t-global.bcs:4444/wd/hub").toURL(),
+//                        desiredCapabilities));
 //    }
 //
 //    public static void closeApp(){
