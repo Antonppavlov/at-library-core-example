c61ecbd15fd502ab03ce5399b8ea43ecbd67e893
Antonppavlov <anton.it.pavlov@gmail.com>
2019-06-17 17:11:41 +0300

    commit

--- src/test/java/ru/bcs/test/demo/dolgov/android/AndroidDriversMethods.java
+++ src/test/java/ru/bcs/test/demo/dolgov/android/AndroidDriversMethods.java
@@ -14,8 +14,8 @@ public class AndroidDriversMethods {
     public static void androidLounchApp() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("platformVersion", "7.0");
-        desiredCapabilities.setCapability("deviceName", "Nexus 5X API 24");
+        desiredCapabilities.setCapability("platformVersion", "8.1.0");
+        desiredCapabilities.setCapability("deviceName", "Nokia 6");
         desiredCapabilities.setCapability("app", "/Users/ivanivanov/Downloads/ru.bcs.premier_1.11.2.apk");
         setWebDriver(
                 new RemoteWebDriver(
@@ -29,3 +29,6 @@ public class AndroidDriversMethods {
     }
 
 }
+//
+//<browser name="Nokia 6" defaultVersion="8.1.0">
+//<version number="8.1.0">
